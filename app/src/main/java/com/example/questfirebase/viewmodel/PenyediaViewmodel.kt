82b5da37aa88be5be8ab90473e0c9ac22558a373
axen4u <damaraarya1303@gmail.com>
2026-01-10 01:23:02 +0700

    Implement DetailViewModel Provider

--- app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewmodel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewmodel.kt
@@ -5,3 +5,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.myfirebase.repositori.RepositorySiswa
 
 class PenyediaViewModel(private val repository: RepositorySiswa) : ViewModelProvider.Factory {
+    // File: PenyediaViewModel.kt (Step 2)
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(DetailViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return DetailViewModel(repository) as T
+        }
+        // ...
+    }
