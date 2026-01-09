d8e313196ab1789795a3843cc76049e99259eeb3
axen4u <damaraarya1303@gmail.com>
2026-01-10 01:13:17 +0700

    Setup EditViewModel and Dependency Injection

--- app/src/main/java/com/example/questfirebase/viewmodel/Editviewmodel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/Editviewmodel.kt
@@ -1,2 +1,27 @@
-package com.example.questfirebase.viewmodel
+package com.example.myfirebase.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.DetailSiswa
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import com.example.myfirebase.modeldata.toUIStateSiswa
+import com.example.myfirebase.repositori.RepositorySiswa
+import com.example.myfirebase.view.route.DestinasiNavigasi
+import kotlinx.coroutines.launch
+
+// File: EditViewModel.kt (Part 1)
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+    private val siswaId: String = checkNotNull(
+        savedStateHandle[DestinasiNavigasi.Edit.ARG_ID]
+    )
+
 
