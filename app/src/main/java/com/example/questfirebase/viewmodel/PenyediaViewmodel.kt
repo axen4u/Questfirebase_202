17e89724528abe97ba656f9de2c32bb6ab051750
axen4u <damaraarya1303@gmail.com>
2026-01-10 01:21:51 +0700

    Setup Factory Class & Dependency Injection

--- app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewmodel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/PenyediaViewmodel.kt
@@ -1,2 +1,7 @@
-package com.example.questfirebase.viewmodel
+package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.myfirebase.repositori.RepositorySiswa
+
+class PenyediaViewModel(private val repository: RepositorySiswa) : ViewModelProvider.Factory {
