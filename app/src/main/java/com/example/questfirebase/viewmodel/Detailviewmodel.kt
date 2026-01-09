8aa3bed2054747e0cba0b9cd66a219a4c2814d0e
axen4u <damaraarya1303@gmail.com>
2026-01-10 01:08:04 +0700

    Setup ViewModel & State Initialization

--- app/src/main/java/com/example/questfirebase/viewmodel/Detailviewmodel.kt
+++ app/src/main/java/com/example/questfirebase/viewmodel/Detailviewmodel.kt
@@ -1,2 +1,19 @@
-package com.example.questfirebase.viewmodel
+package com.example.myfirebase.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myfirebase.modeldata.Siswa
+import com.example.myfirebase.modeldata.toUIStateSiswa
+import com.example.myfirebase.repositori.ContainerApp
+import com.example.myfirebase.repositori.RepositorySiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class DetailViewModel : ViewModel() {
+
+    private val repository: RepositorySiswa = ContainerApp.getInstance().firebaseRepositorySiswa
+
+    private val _uiState = MutableStateFlow<UIStateSiswa>(UIStateSiswa())
+    val uiState: StateFlow<UIStateSiswa> = _uiState
 
