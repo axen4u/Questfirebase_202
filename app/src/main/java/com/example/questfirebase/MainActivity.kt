c76255302895040cb899e32004277f58ebce8756
axen4u <damaraarya1303@gmail.com>
2026-01-10 00:59:24 +0700

    Add preview annotation for Greeting composable

--- app/src/main/java/com/example/questfirebase/MainActivity.kt
+++ app/src/main/java/com/example/questfirebase/MainActivity.kt
@@ -4,25 +4,45 @@ package com.example.questfirebase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.questfirebase.ui.theme.QuestFirebaseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            // Will add UI later
+            QuestFirebaseTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
         }
     }
 }
-// Perubahan:
-// - Tambahkan enableEdgeToEdge()
-// - Bungkus setContent dengan QuestFirebaseTheme
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    enableEdgeToEdge()
-    setContent {
-        QuestFirebaseTheme {
-            // Content goes here
-        }
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuestFirebaseTheme {
+        Greeting("Android")
     }
 }
